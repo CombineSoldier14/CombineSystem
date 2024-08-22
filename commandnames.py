14f2f4a0fbfbfd74d0bfb98bcf4cff94fe8d871d
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-08-21 20:35:19 -0700

    Update commandnames.py

--- commandnames.py
+++ commandnames.py
@@ -1,7 +1,12 @@
 from commands.cowsay import cowsaycmd
 from commands.help import help
+from commands.ls import ls
+from commands.cd import cd
+from commands.read import read
+from commands.osmode import osmode
+from commands.wget import wgetcmd
 
-cmdnames = ["cowsay", "help"]
+cmdnames = ["cowsay", "help", "ls", "cd", "read", "wget", "osmode"]
 
 cmds = [
     {
@@ -11,5 +16,25 @@ cmds = [
     {
         "name":"help",
         "func":help
+    },
+    {
+        "name":"ls",
+        "func":ls
+    },
+    {
+        "name":"cd",
+        "func":cd
+    },
+    {
+        "name":"read",
+        "func":read
+    },
+    {
+        "name":"osmode",
+        "func":osmode
+    },
+    {
+        "name":"wget",
+        "func":wgetcmd
     }
 ]
