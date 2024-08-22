6a4f97709bdd648e6b1e8853359805c089f175f9
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-08-21 20:36:54 -0700

    Update help.py

--- commands/help.py
+++ commands/help.py
@@ -6,5 +6,10 @@ def help():
     Commands:
     {colored("cowsay", "light_blue", attrs=["bold"])} - get ASCII art of a cow saying your text!\n
     {colored("help", "light_blue", attrs=["bold"])} - get help on commands! {colored("which you probably already figured out...", "white", attrs=["dark"])}\n
+    {colored("ls", "light_blue", attrs=["bold"])} - show the files and folders in the current directory!\n
+    {colored("cd", "light_blue", attrs=["bold"])} - change the directory you are in\n
+    {colored("read", "light_blue", attrs=["bold"])} - read a text file!\n
+    {colored("osmode", "light_blue", attrs=["bold"])} - run a command in OS mode, meaning it will be handled by your base OS and not CombineSystem\n
+    {colored("wget", "light_blue", attrs=["bold"])} - download files from URL\n
     """
     print(text)
