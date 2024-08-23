e184a2c5482f4796d976efd285f666a7c97041bd
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-08-22 17:26:35 -0700

    Update osinterpreter.py

--- osinterpreter.py
+++ osinterpreter.py
@@ -8,10 +8,8 @@ with open("/CombineSystemImg/CombineSystem/config.json") as e:
     config = json.load(e)
 
 def interpreter():
-    try:
-        r = input(f"{colored("CombineSystem", "green", attrs=["bold"])} v{config["VERSION"]} {colored("[", "magenta")}{colored(os.getcwd(), "light_blue")}{colored("]", "magenta")} -> ")
-    except EOFError:
-        return
+  
+    r = input(f"{colored("CombineSystem", "green", attrs=["bold"])} v{config["VERSION"]} {colored("[", "magenta")}{colored(os.getcwd(), "light_blue")}{colored("]", "magenta")} -> ")
     if r.replace(' ', '').lower() not in cmdnames:
         print(f"{colored("Error", "red", attrs=["bold"])}: The command \"{colored(x, "light_blue", attrs=["bold"])}\" was not found. Maybe you misspelled it?")
     else:
