ae92b81bb6cb720ef910ccf20d591d74941e2085
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-08-22 17:36:01 -0700

    Update osinterpreter.py

--- osinterpreter.py
+++ osinterpreter.py
@@ -4,16 +4,20 @@ from commandnames import cmdnames
 import json
 import os
 
+with open("config.json") as e:
 with open("/CombineSystemImg/CombineSystem/config.json") as e:
     config = json.load(e)
 
 def interpreter():
-  
-    r = input(f"{colored("CombineSystem", "green", attrs=["bold"])} v{config["VERSION"]} {colored("[", "magenta")}{colored(os.getcwd(), "light_blue")}{colored("]", "magenta")} -> ")
-    if r.replace(' ', '').lower() not in cmdnames:
+    x = input("{0} v{1} {2}{3}{4} -> ".format(colored("CombineSystem", "green", attrs=["bold"]), 
+                                          config["VERSION"], 
+                                          colored("[", "magenta"),
+                                          colored(os.getcwd(), "light_blue"),
+                                          colored("]", "magenta")))
+    if x.replace(' ', '').lower() not in cmdnames:
         print(f"{colored("Error", "red", attrs=["bold"])}: The command \"{colored(x, "light_blue", attrs=["bold"])}\" was not found. Maybe you misspelled it?")
     else:
         for y in cmds:
-            if r.replace(' ', '').lower() == y["name"]:
+            if x.replace(' ', '').lower() == y["name"]:
                 y["func"]()
     interpreter()
