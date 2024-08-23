7b5eec6ed9e0831cf88c57134ef89e6d0577c561
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-08-22 17:17:16 -0700

    Update osinterpreter.py

--- osinterpreter.py
+++ osinterpreter.py
@@ -8,15 +8,15 @@ with open("/CombineSystemImg/CombineSystem/config.json") as e:
     config = json.load(e)
 
 def interpreter():
-    x = input("{0} v{1} {2}{3}{4} -> ".format(colored("CombineSystem", "green", attrs=["bold"]), 
+    r = input("{0} v{1} {2}{3}{4} -> ".format(colored("CombineSystem", "green", attrs=["bold"]), 
                                           config["VERSION"], 
                                           colored("[", "magenta"),
                                           colored(os.getcwd(), "light_blue"),
                                           colored("]", "magenta")))
-    if x.replace(' ', '').lower() not in cmdnames:
+    if r.replace(' ', '').lower() not in cmdnames:
         print(f"{colored("Error", "red", attrs=["bold"])}: The command \"{colored(x, "light_blue", attrs=["bold"])}\" was not found. Maybe you misspelled it?")
     else:
         for y in cmds:
-            if x.replace(' ', '').lower() == y["name"]:
+            if r.replace(' ', '').lower() == y["name"]:
                 y["func"]()
     interpreter()
